3b7c5021aee8972c89bd59526bbc204621013706
silvervat <silver.vatsel@gmail.com>
2025-10-24 20:57:43 +0200

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/trimble-markup-extension/',  // Repo nimega (sinu puhul "trimble-markup-ext") – see lahendab asset 404-d
+  base: '/trimble-markup-ext/',  // Repo nimega (sinu puhul "trimble-markup-ext") – see lahendab asset 404-d
   define: {
     global: 'globalThis',
   },
