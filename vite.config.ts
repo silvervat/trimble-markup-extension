8fb2acca45ea6d1774a117aba968bc4b292ab6ca
silvervat <silver.vatsel@gmail.com>
2025-10-24 20:57:20 +0200

    Change base path for Vite configuration
    Updated base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Oluline GitHub Pages'i jaoks
+  base: '/trimble-markup-extension/',  // Repo nimega (sinu puhul "trimble-markup-ext") – see lahendab asset 404-d
   define: {
     global: 'globalThis',
   },
